d065ea6717f88d085980f3d1102a864b39601d80
emdash <dotsony@gmail.com>
2021-07-31 14:16:37 -0700

    Also include store feature on alpha keypads

--- mikescalc.js
+++ mikescalc.js
@@ -744,7 +744,7 @@ function app(element) {
 	"q w e r t y u i o p",
 	". a s d f g h j k l",
 	". . z x c v b n m .",
-	". . . # # # # # . .",
+	". . . = = # # # . .",
     );
 
     const A = layout(
@@ -752,7 +752,7 @@ function app(element) {
 	"Q W E R T Y U I O P",
 	". A S D F G H J K L",
 	". . Z X C V B N M .",
-	". . . # # # # # . .",
+	". . . = = # # # . .",
     );
     
     // Layout consisting of all available functions.
